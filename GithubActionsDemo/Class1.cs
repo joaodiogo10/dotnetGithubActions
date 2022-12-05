2fca8ee9c453ee47bb511d75c46560fe336fb38c
joaodiogoPic <96116442+joaodiogoPic@users.noreply.github.com>
2022-12-05 22:47:12 +0000

    Test deploymente

--- GithubActionsDemo/Class1.cs
+++ GithubActionsDemo/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace GithubActionsDemo
 {
+    class Person
+    {
+        string name;
+        int age;
+    }
+
     public class Class1
     {
+
+        void Test()
+        {
+            Person aPerson = new Person();
+            string json = JsonConvert.SerializeObject(aPerson);
+        }
+
     }
 }
